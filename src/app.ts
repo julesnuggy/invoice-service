4a0217159f15bc8730f41891e6d86266a2f8a4be
julesnuggy <julesnuggy@gmail.com>
2020-02-24 21:39:39 +0000

    Setup basic express server

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+
+// Controllers (route handlers)
+import * as indexController from "./controllers/index";
+
+// Create Express server
+const app = express();
+
+// Express configuration
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
+);
+
+/**
+ * Primary app routes.
+ */
+app.get("/", indexController.index);
+
+export default app;
