ea6c3d051c006cdf99e7c7895b05aacb35f3931e
julesnuggy <julesnuggy@gmail.com>
2020-02-26 23:23:12 +0000

    Setup some GET and POST queries to DB

--- src/controllers/createInvoice.ts
+++ src/controllers/createInvoice.ts
@@ -1,10 +1,52 @@
 import { Request, Response } from "express";
+import { Pool } from "pg";
+
+const invoicingClient = new Pool({
+  user: "invoicinguser",
+  password: "password",
+  host: "localhost",
+  port: 5432,
+  database: "invoicing"
+});
 
 /**
  * POST /
  * Create Invoice
  */
 export const create = (req: Request, res: Response ) => {
-  console.log("create invoice request", req.body);
-  res.send("Create invoice POST response");
+  req.body.invoiceLineItems.map((line: any) => {
+    const { item, quantity, price, total } = line;
+
+    // qq - need to create invoice first due to PKEY/FKEY constraint
+    /**
+     BEGIN TRANSACTION;
+     INSERT INTO invoices(merchant_id, customer_id, subtotal, discount, grand_total, status) VALUES (
+     (SELECT id FROM users WHERE name=$1),
+     (SELECT id FROM users WHERE name=$2),
+     $3,
+     $4,
+     $5,
+     $6
+     );
+
+     INSERT INTO invoice_lines(invoice_id, item, quantity, price, total) VALUES (
+     'Get invoice_id from return value of results.row from previous query',
+     $1,
+     $2,
+     $3,
+     $4
+     );
+
+     COMMIT;
+     */
+
+    invoicingClient.query("INSERT INTO invoice_lines (item, quantity, price, total) VALUES ($1, $2, $3, $4)",
+    [item, quantity, price, total],
+    (err, result) => {
+      if(err) {
+        throw err;
+      }
+      res.status(201).send(`Successfully created new invoice: ${result}`);
+    });
+  });
 };
---
+++ src/controllers/user.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+import { Pool } from "pg";
+
+const invoicingClient = new Pool({
+  user: "invoicinguser",
+  password: "password",
+  host: "localhost",
+  port: 5432,
+  database: "invoicing"
+});
+
+/**
+ * POST /
+ * Create User
+ */
+export const create = (req: Request, res: Response ) => {
+  const { type, name, balance } = req.body;
+
+  invoicingClient.query("INSERT INTO users (type, name, balance) VALUES ($1, $2, $3)",
+    [type, name, balance],
+    (err, result) => {
+      if(err) {
+        console.error(err);
+      }
+      res.status(201).send(`Successfully created new user`);
+    });
+};
+
+export const getAll = (req: Request, res: Response ) => {
+  console.log("req.body", req.body);
+
+  invoicingClient.query("SELECT * FROM users",
+    (err, result) => {
+      if(err) {
+        console.error(err);
+      }
+      res.status(200).json(result.rows);
+    });
+};
